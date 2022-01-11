22af37e82baef4e3800ffe24ce04c4ee9296d6d4
James Johnson <johnsojr@umn.edu>
2022-01-11 15:22:39 -0600

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-  },
-  extends: ["eslint:recommended", "plugin:vue/essential"],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-  },
-  plugins: ["vue"],
-  rules: {},
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
 };
